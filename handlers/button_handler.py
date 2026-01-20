1959169aeee5cf630f5ffcb3b1da14f2ae8289c0
Osif ungar <osif.e.u@gmail.com>
2026-01-20 23:38:04 +0200

    Update button_handler.py

--- handlers/button_handler.py
+++ handlers/button_handler.py
@@ -1,6 +1,10 @@
 from utils.send_message import send_message
 
 def handle_button(chat_id, callback_data):
+    print("Button pressed:", callback_data)
+
     if callback_data == "button_pressed":
         text = "הכפתור נלחץ! הנה ההודעה שחוזרת."
         return send_message(chat_id, text)
+
+    return send_message(chat_id, "לא זיהיתי את הכפתור.")

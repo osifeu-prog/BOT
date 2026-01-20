46f041ef7c8d5bf56445183a62db8a287b94d4a9
Osif ungar <osif.e.u@gmail.com>
2026-01-21 00:25:24 +0200

    Update and rename env.example to keyboards/second_stage.py

---
+++ keyboards/second_stage.py
@@ -0,0 +1,9 @@
+def second_stage_buttons():
+    return {
+        "inline_keyboard": [
+            [
+                {"text": "אפשרות 1", "callback_data": "option_1"},
+                {"text": "אפשרות 2", "callback_data": "option_2"}
+            ]
+        ]
+    }

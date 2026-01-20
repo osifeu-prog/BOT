fd9bd93f5ede67b83e981d7b7b0d4fc58a6130d0
Osif ungar <osif.e.u@gmail.com>
2026-01-20 23:30:39 +0200

    Create main_menu.py

---
+++ keyboards/main_menu.py
@@ -0,0 +1,8 @@
+def main_menu_keyboard():
+    return {
+        "inline_keyboard": [
+            [
+                {"text": "לחץ כאן", "callback_data": "button_pressed"}
+            ]
+        ]
+    }

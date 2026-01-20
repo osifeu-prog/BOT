a7ee6a1fa21375b53224283448a2a386fb6b30f4
Osif ungar <osif.e.u@gmail.com>
2026-01-21 00:46:43 +0200

    Delete handlers directory

--- handlers/start_handler.py
+++
@@ -1,9 +0,0 @@
-from utils.send_message import send_message
-from keyboards.first_button import first_button
-
-def handle_start(chat_id):
-    return send_message(
-        chat_id,
-        "ברוך הבא! לחץ על הכפתור כדי להתחיל.",
-        reply_markup=first_button()
-    )

6519c8942878ff088e469a66f4d7d0f13d75366a
Osif ungar <osif.e.u@gmail.com>
2026-01-21 00:24:13 +0200

    Delete handlers directory

--- handlers/start_handler.py
+++
@@ -1,6 +0,0 @@
-from keyboards.main_menu import main_menu_keyboard
-from utils.send_message import send_message
-
-def handle_start(chat_id):
-    text = "ברוך הבא! לחץ על הכפתור כדי להמשיך."
-    return send_message(chat_id, text, reply_markup=main_menu_keyboard())

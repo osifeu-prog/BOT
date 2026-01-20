847db1f94e148fe503ab5bae84af097286b1562a
Osif ungar <osif.e.u@gmail.com>
2026-01-21 00:25:01 +0200

    Update and rename main.py to keyboards/first_button.py

---
+++ keyboards/first_button.py
@@ -0,0 +1,8 @@
+def first_button():
+    return {
+        "inline_keyboard": [
+            [
+                {"text": "לחץ להתחלה", "callback_data": "start_flow"}
+            ]
+        ]
+    }

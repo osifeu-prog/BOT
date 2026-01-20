003025b69459806e2388d907896a72316aa90324
Osif ungar <osif.e.u@gmail.com>
2026-01-20 15:33:07 +0200

    Create templates.py

---
+++ app/templates.py
@@ -0,0 +1,13 @@
+# This file can contain message templates if needed
+# For now, it's left empty as we're using inline messages
+
+WELCOME_MESSAGE = """
+🎰 **ברוך הבא ל-NFTY ULTRA CASINO!** 🎰
+
+👤 **משתמש:** {first_name}
+💎 **דרגה:** {tier}
+💰 **יתרה:** {balance} מטבעות
+👥 **הפניות:** {referrals} משתמשים
+
+🎮 **בחר משחק:**
+"""

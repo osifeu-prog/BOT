6fdf5827cb9988d3736036883373ba1370ad986b
Osif ungar <osif.e.u@gmail.com>
2026-01-21 12:36:53 +0200

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,3 +1,15 @@
+"""
+db/connection.py
+=================
+אחראי על יצירת חיבור למסד הנתונים PostgreSQL.
+
+כל פונקציה שעובדת מול DB:
+- קוראת ל-get_conn()
+- יוצרת cursor
+- מריצה שאילתות
+- סוגרת חיבור
+"""
+
 import psycopg2
 from utils.config import DB_URL
 

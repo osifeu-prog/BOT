3e01289da206fb41d217ddf7681a8a81e7c86e95
Osif ungar <osif.e.u@gmail.com>
2026-01-21 11:37:52 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,7 @@
-from utils.telegram import send_message
+from utils.telegram import send_message, send_photo
 from texts.messages import TEXTS
 from buttons.menus import BUTTONS
+from utils.photos import START_PHOTO_URL
 from db.events import log_event
 
 async def start_handler(chat):
@@ -9,6 +10,10 @@ async def start_handler(chat):
 
     log_event(user_id, "command", "/start")
 
+    # שליחת תמונה
+    await send_photo(user_id, START_PHOTO_URL)
+
+    # שליחת טקסט + תפריט
     text = TEXTS["welcome"].format(name=name)
     reply_markup = {"inline_keyboard": [BUTTONS["main_menu"]]}
 

fbd99ccbe7fba6b6bee9c7c1369d9c24371d62f2
Osif ungar <osif.e.u@gmail.com>
2026-01-21 11:35:47 +0200

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -12,3 +12,16 @@ async def send_message(chat_id, text, reply_markup=None):
 
     async with httpx.AsyncClient() as client:
         await client.post(f"{API_URL}/sendMessage", json=payload)
+
+
+async def send_photo(chat_id, photo_url, caption=None):
+    payload = {
+        "chat_id": chat_id,
+        "photo": photo_url
+    }
+
+    if caption:
+        payload["caption"] = caption
+
+    async with httpx.AsyncClient() as client:
+        await client.post(f"{API_URL}/sendPhoto", json=payload)

1adcbcd07dc5373134a7cc014316c8556a60db3f
Osif ungar <osif.e.u@gmail.com>
2026-01-21 12:23:06 +0200

    Update menu.py

--- callbacks/menu.py
+++ callbacks/menu.py
@@ -1,14 +1,34 @@
 from utils.telegram import send_message
-from texts.payment import PAYMENT_MESSAGE
+from texts.payment import get_payment_message
+from texts.how_it_works import HOW_IT_WORKS
+from texts.telegram_ui import TELEGRAM_UI_EXPLAINER
 from db.events import log_event
+from handlers.slots import play_slots, show_leaderboard
 
 async def menu_callback(callback):
     user_id = callback["message"]["chat"]["id"]
     data = callback["data"]
+    chat = callback["message"]["chat"]
 
     log_event(user_id, "button", data)
 
     if data == "menu_buy":
-        return await send_message(user_id, PAYMENT_MESSAGE)
+        msg = get_payment_message()
+        return await send_message(user_id, msg)
+
+    if data == "menu_how":
+        return await send_message(user_id, HOW_IT_WORKS)
+
+    if data == "menu_ui":
+        return await send_message(user_id, TELEGRAM_UI_EXPLAINER)
+
+    if data == "menu_slots":
+        return await play_slots(chat)
+
+    if data == "menu_leaders":
+        return await show_leaderboard(chat)
+
+    if data == "menu_help":
+        return await send_message(user_id, "לתמיכה: @osifeu_prog")
 
     return await send_message(user_id, f"נבחר: {data}")

571a01beff1e239000080d907736b3dab7c960df
Osif ungar <osif.e.u@gmail.com>
2026-01-21 12:20:52 +0200

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -4,9 +4,9 @@ from utils.config import API_URL
 async def send_message(chat_id, text, reply_markup=None):
     payload = {
         "chat_id": chat_id,
-        "text": text
+        "text": text,
+        "parse_mode": "Markdown"
     }
-
     if reply_markup:
         payload["reply_markup"] = reply_markup
 
@@ -14,14 +14,16 @@ async def send_message(chat_id, text, reply_markup=None):
         await client.post(f"{API_URL}/sendMessage", json=payload)
 
 
-async def send_photo(chat_id, photo_url, caption=None):
+async def send_photo(chat_id, photo_url, caption=None, reply_markup=None):
     payload = {
         "chat_id": chat_id,
         "photo": photo_url
     }
-
     if caption:
         payload["caption"] = caption
+        payload["parse_mode"] = "Markdown"
+    if reply_markup:
+        payload["reply_markup"] = reply_markup
 
     async with httpx.AsyncClient() as client:
         await client.post(f"{API_URL}/sendPhoto", json=payload)

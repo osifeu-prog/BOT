bc15f28dd69cd6064d1ce40c479cd4d2357a91ab
Osif ungar <osif.e.u@gmail.com>
2026-01-21 11:28:49 +0200

    Update callback_router.py

--- handlers/callback_router.py
+++ handlers/callback_router.py
@@ -1,13 +1,14 @@
 from callbacks.menu import menu_callback
-from utils.config import ALLOWED_USERS
+from db.admins import is_admin
+from utils.config import ADMIN_ID
 
 async def handle_callback(callback):
     user_id = callback["message"]["chat"]["id"]
-
-    if user_id not in ALLOWED_USERS:
-        return
-
     data = callback["data"]
+
+    # רק מנהלים יכולים ללחוץ על כפתורים
+    if not is_admin(user_id) and user_id != ADMIN_ID:
+        return
 
     if data.startswith("menu_"):
         return await menu_callback(callback)

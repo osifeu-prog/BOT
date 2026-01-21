4b2e5cb9be5749de681ad46ccd64e8bc601e7bb5
Osif ungar <osif.e.u@gmail.com>
2026-01-21 11:34:25 +0200

    Create payment.py

---
+++ texts/payment.py
@@ -0,0 +1,12 @@
+from utils.config import PRICE_SH, TON_WALLET
+
+PAYMENT_MESSAGE = f"""
+כדי לקבל את קובץ ה־ZIP של הפרויקט:
+
+💰 עלות: {PRICE_SH} ש"ח  
+💎 תשלום דרך TON:  
+{TON_WALLET}
+
+לאחר התשלום, שלח לי צילום מסך של ההעברה,  
+והמערכת תאשר אותך אוטומטית.
+"""

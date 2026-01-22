f41d9094a2d6a0298e84281e73abd3c3d776821b
osifeu-prog <you@example.com>
2026-01-22 19:34:14 +0200

    🔧 CRITICAL: Fixed missing Participants link and import errors

--- utils/config.py
+++ utils/config.py
@@ -6,4 +6,6 @@ ADMIN_ID = os.getenv("ADMIN_ID")
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "osifeu_prog")
 TON_WALLET = os.getenv("TON_WALLET")
 PRICE_SH = os.getenv("PRICE_SH", "99")
+TEST_GROUP_LINK = os.getenv("TEST_GROUP_LINK", "https://t.me/+2zNbLBrgrQkwZDhk")
+PARTICIPANTS_GROUP_LINK = os.getenv("TEST_GROUP_LINK") # נשתמש באותו קישור כברירת מחדל
 PORT = int(os.getenv("PORT", 8080))
